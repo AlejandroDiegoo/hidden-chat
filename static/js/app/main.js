8b27e687d08309d11f4e86bd0d89543ecf007cb7
Alejandro <cuentas@alejandrodiego.com>
2017-02-01 09:31:14 +0100

    Update main.js

--- static/js/app/main.js
+++ static/js/app/main.js
@@ -4,11 +4,11 @@ requirejs.config({
 
         domReady: '../vendor/domReady',
         jquery: '../vendor/jquery-2.1.3.min',
-    		underscore: '../vendor/underscore.min',
-    		backbone: '../vendor/backbone.min',
-    		marionette: '../vendor/backbone.marionette',
-    		tpl: '../vendor/underscore.tpl',
-    		text: '../vendor/text',
+		underscore: '../vendor/underscore.min',
+		backbone: '../vendor/backbone.min',
+		marionette: '../vendor/backbone.marionette',
+		tpl: '../vendor/underscore.tpl',
+		text: '../vendor/text',
         websocket: 'websocket'
 
     }
@@ -16,56 +16,31 @@ requirejs.config({
 });
 
 require([
+	'jquery',
+	'domReady',
+	'app',
+	'backbone',
+	'underscore'
+], function($, domReady, app) {
 
-  	'jquery',
-  	'domReady',
-  	'backbone',
-  	'underscore'
+	var hiddenChatConsole = {};
+	
+	$.extend(true, hiddenChatConsole, console);
 
-], function($, domReady) {
+	for (consoleFunction in console) {
+		if (consoleFunction != 'clear' && typeof console[consoleFunction] === 'function') {
+          console[consoleFunction] = function() {};
+		}
+	}
 
+	window.onerror = function(message, url, lineNumber) {
+		return true;
+	};
+	
+	window.hiddenChatConsole = hiddenChatConsole;
+	hiddenChatConsole.clear();
 
-  var hiddenChatConsole = {};
-
-$.extend(true, hiddenChatConsole, console);
-
-
-
-  for (hola in console) {
-      if (hola != 'clear' && typeof console[hola] === 'function') {
-          console[hola] = function() {};
-      }
-  }
-
-  window.onerror = function(message, url, lineNumber) {
-          // code to execute on an error
-          return true; // prevents browser error messages
-      };
-
-  window.hiddenChatConsole = hiddenChatConsole;
-hiddenChatConsole.clear();
-
-
-
-
-document.addEventListener('touchmove', function (e) {
-
-
-e.preventDefault(); }, false);
-
-
-
-
-	require([
-		'app'
-	], function(app) {
-		'use strict';
-
-    window.App = app;
-		//setTimeout( function() {
-			app.start();
-		//}, 1000);
-
-	});
+	window.App = app;
+	app.start();
 
 });
