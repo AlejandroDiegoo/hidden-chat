4f69b23e4f8ac990dcfcd9405b572341fb9a4df4
Alejandro <cuentas@alejandrodiego.com>
2017-02-01 10:02:05 +0100

    Update websocket.js

--- static/js/app/websocket.js
+++ static/js/app/websocket.js
@@ -1,139 +1,138 @@
 define([
-    'jquery',
-    'app'
+	'jquery',
+	'app'
 ], function($, App) {
-    'use strict';
+	'use strict';
 
-    var websocket = {
+	var websocket = {
 
-        _socket: null,
+		_socket: null,
 
-        connection: function(onOpen, onError, onMessage) {
+		connection: function(onOpen, onError, onMessage) {
 
-            websocket.close();
-            websocket._socket = new WebSocket('ws://' + document.domain + ':' + location.port, ['chat']);
-            websocket._socket.onopen = (onOpen) ? onOpen.bind(this) : websocket.onOpen.bind(this);
-            websocket._socket.onerror = (onError) ? onError.bind(this) : websocket.onError.bind(this);
-            websocket._socket.onmessage = (onMessage) ? onMessage.bind(this) : websocket.onMessage.bind(this);
+			websocket.close();
+			websocket._socket = new WebSocket('ws://' + document.domain + ':' + location.port, ['chat']);
+			websocket._socket.onopen = (onOpen) ? onOpen.bind(this) : websocket.onOpen.bind(this);
+			websocket._socket.onerror = (onError) ? onError.bind(this) : websocket.onError.bind(this);
+			websocket._socket.onmessage = (onMessage) ? onMessage.bind(this) : websocket.onMessage.bind(this);
 
-        },
+		},
 
-        onOpen: function(response) {
+		onOpen: function(response) {
 
-            App.trigger('WEBSOCKET:CONNECTION:SUCCESS', response);
+			App.trigger('WEBSOCKET:CONNECTION:SUCCESS', response);
 
-        },
+		},
 
-        onError: function(response) {
+		onError: function(response) {
 
-            App.trigger('WEBSOCKET:CONNECTION:ERROR', response);
+			App.trigger('WEBSOCKET:CONNECTION:ERROR', response);
 
-        },
+		},
 
-        onMessage: function(response) {
+		onMessage: function(response) {
 
-            App.trigger('WEBSOCKET:MESSAGE', response);
+			App.trigger('WEBSOCKET:MESSAGE', response);
 
-            response = JSON.parse(response.data);
-            var responseTimeHours = new Date(response.date).getHours();
-            var responseTimeMinutes = new Date(response.date).getMinutes();
-            var responseTimeSeconds = new Date(response.date).getSeconds();
+			response = JSON.parse(response.data);
+			var responseTimeHours = new Date(response.date).getHours();
+			var responseTimeMinutes = new Date(response.date).getMinutes();
+			var responseTimeSeconds = new Date(response.date).getSeconds();
 
-            if (response.type == 'source') {
+			if (response.type == 'source') {
 
-                this.setSource(response);
+				this.setSource(response);
 
-            } else if (response.type == 'users') {
+			} else if (response.type == 'users') {
 
-                var usersString = '';
+				var usersString = '';
 
-                $.each(response.message, function(key, value) {
+				$.each(response.message, function(key, value) {
 
-                    if (key > 0) {
-                        usersString += ', ';
-                    }
+					if (key > 0) {
+						usersString += ', ';
+					}
 
-                    usersString += value;
+					usersString += value;
 
-                });
+				});
 
-                hiddenChatConsole.log('%cUSERS ONLINE: %c%s', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;', usersString);
+				hiddenChatConsole.log('%cUSERS ONLINE: %c%s', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;', usersString);
 
-            } else if (response.type == 'connection') {
+			} else if (response.type == 'connection') {
 
-                if (response.owner) {
+				if (response.owner) {
 
-                    hiddenChatConsole.log('%cWELCOME TO HIDDEN-CHAT', 'background-color: #196d83; padding: 0px 5px; color: #ffffff; font-size: 120%;');
-                    hiddenChatConsole.log('%cList users: %c/users', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
-                    hiddenChatConsole.log('%cChange username: %c/nick [USERNAME]', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
-                    hiddenChatConsole.log('%cChange source: %c/source [domain.com]', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
+					hiddenChatConsole.log('%cWELCOME TO HIDDEN-CHAT', 'background-color: #196d83; padding: 0px 5px; color: #ffffff; font-size: 120%;');
+					hiddenChatConsole.log('%cList users: %c/users', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
+					hiddenChatConsole.log('%cChange username: %c/nick [USERNAME]', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
+					hiddenChatConsole.log('%cChange source: %c/source [domain.com]', 'font-size: 80%; color: #BBB;', 'font-size: 80%; color: #AAA;');
 
-                } else {
+				} else {
 
-                    hiddenChatConsole.log('%c[%s:%s:%s] %s has joined the room', 'font-size: 80%; color: #CCC;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, response.user);
+					hiddenChatConsole.log('%c[%s:%s:%s] %s has joined the room', 'font-size: 80%; color: #CCC;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, response.user);
 
-                }
+				}
 
-            } else if (response.type == 'disconnection') {
+			} else if (response.type == 'disconnection') {
 
-                hiddenChatConsole.log('%c[%s:%s:%s] %s has left the room', 'font-size: 80%; color: #CCC;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, response.user);
+				hiddenChatConsole.log('%c[%s:%s:%s] %s has left the room', 'font-size: 80%; color: #CCC;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, response.user);
 
-            } else if (response.type == 'nick') {
+			} else if (response.type == 'nick') {
 
-                  hiddenChatConsole.log('%c%s', 'font-size: 80%; color: #CCC;', response.message);
+				hiddenChatConsole.log('%c%s', 'font-size: 80%; color: #CCC;', response.message);
 
-            } else {
+			} else {
 
-                var styleOwner = (response.owner) ? 'background-color: #77afbd; padding: 0px 1px; color: #ffffff;' : '';
+				var styleOwner = (response.owner) ? 'background-color: #77afbd; padding: 0px 1px; color: #ffffff;' : '';
 
-                hiddenChatConsole.log('%c[%s:%s:%s] %c%s%c %s', 'color: #BBB;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, 'color: #77afbd; font-weight: bold;' + styleOwner, response.user, 'color: #777;', response.message);
+				hiddenChatConsole.log('%c[%s:%s:%s] %c%s%c %s', 'color: #BBB;', responseTimeHours, responseTimeMinutes, responseTimeSeconds, 'color: #77afbd; font-weight: bold;' + styleOwner, response.user, 'color: #777;', response.message);
 
-            }
+			}
 
-        },
+		},
 
-        setSource(response) {
+		setSource(response) {
 
-            var source = '';
-            $.each(response.message, function(key, value) {
-                source += source;
-            });
+			var source = '';
+			$.each(response.message, function(key, value) {
+				source += source;
+			});
 
-            var iframe = document.getElementById('source');
-            var iframeWindow = iframe.contentWindow;
-            iframeWindow.console = console;
-            
-            // Prevents iframe error messages
-            iframeWindow.onerror = function(message, url, lineNumber) {
-                return true;
-            };
-            
-            // Prevents browser error messages
-            window.onerror = function(message, url, lineNumber) {
-                return true;
-            };
+			var iframe = document.getElementById('source');
+			var iframeWindow = iframe.contentWindow;
+			iframeWindow.console = console;
 
-            var iframeHtml = $('body', iframe.contentWindow.document);
-            iframeHtml.html(valor);
-            
-        },
+			// Prevents iframe error messages
+			iframeWindow.onerror = function(message, url, lineNumber) {
+				return true;
+			};
 
-        close: function() {
+			// Prevents browser error messages
+			window.onerror = function(message, url, lineNumber) {
+				return true;
+			};
 
-            if (websocket._socket) {
+			var iframeHtml = $('body', iframe.contentWindow.document);
+			iframeHtml.html(valor);
 
-                websocket._socket.close();
+		},
 
-                App.trigger('WEBSOCKET:CLOSE');
+		close: function() {
 
-            }
+			if (websocket._socket) {
 
-        }
+				websocket._socket.close();
 
-    };
+				App.trigger('WEBSOCKET:CLOSE');
 
-    window.websocket = websocket;
+			}
+		}
 
-    return websocket;
+	};
+
+	window.websocket = websocket;
+
+	return websocket;
 
 });
